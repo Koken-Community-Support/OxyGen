53af6d98f3fc9c1f71beaa97dbda0fc3d964160b
Bjarne Varöystrand <black.skorpio+github@gmail.com>
2015-07-16 14:26:33 +0200

    Prep for the new menu options

--- inc/js/main.js
+++ inc/js/main.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
 	$(".up").click(function() {
-		$("html, body").animate({ scrollTop: 0 }, "slow");
+		$("html, body").animate({ 
+			scrollTop: 0 
+		}, "slow");
 		return false;
 	});
 
-	$('.show').click(function () {
+	$('.show').click(function() {
 		$('nav li a').not('.k-nav-current').toggle();
 		$('.show').toggleClass('show-toggled');
 		return false
@@ -15,17 +17,33 @@ $(document).ready(function() {
 		$('#slideshow_nav').hide();
 	};
 
-	function responsiveNav () {
-        if ($(window).width() <= 540){  
+	function responsiveNav() {
+        if ($(window).width() <= 540) {  
             $('nav').addClass('mobile');
             $('.show').addClass('mobileOpen');
-        }   
+        }
     }
     responsiveNav();
 
-    $('.mobileOpen').click(function () {
-    	$('nav.mobile ul').toggle();
+    $('.mobileOpen').click(function() {
+		$('nav.mobile ul').toggle();
     });
+    $("#hover").mouseenter(function() {
+        var main = $('main');
+        var area = $('#hover');
+        var name = $('#sitename')
+        if (!main.hasClass('offset')) {
+            main.addClass('offset');
+            area.addClass('offset');
+            name.addClass('nameoffset');
+        }
+    });
+
+    $("main").mouseenter(function() {
+		$('main').removeClass('offset');
+		$('#hover').removeClass('offset');
+		$('#sitename').removeClass('nameoffset');
+	});
 });
 $(document).on('click', '.cover_scroll_link', function() {
 	$.scrollTo(this.hash, 1000, {easing:'swing'} );
