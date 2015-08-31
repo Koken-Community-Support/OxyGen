d12b3c302ac8a7b075ea346042fc1f0cf74dd4f1
Bjarne Varöystrand <black.skorpio+github@gmail.com>
2015-08-31 14:44:50 +0200

    DEsign update of update message #51
    Added a transparent background image to the whole updatebox.
    Removed the bg-color
    moved update-oxygen-logo.png to the image dir
    Changed the .update-text to white and added some letterspacing.
    Added some placehoders for the future... ;)

--- inc/update.php
+++ inc/update.php
@@ -1,6 +1,8 @@
 <?php
 /*
  * Checking for updates on https://github.com/Koken-Community-Support/OxyGen
+ * Update function created by Christopher (@Hard-One) Bayer
+ * https://github.com/Hard-One
  */
 $ch = curl_init();
 curl_setopt_array($ch, array(
@@ -13,22 +15,26 @@ curl_close($ch);
 $latest = $resp->tag_name; 
 $client = Koken::$site['theme']['version']; // fetching current version
 $downloadLink = "http://kokensupport.varoystrand.se/viewtopic.php?f=16&t=32"; //Link to the OxyGen forum for manual downloads
+$updateLink = "#"; // Link to automatic update (placeholder for the future)
 $themePath = Koken::$location['theme_path']; // get path to themes root directory
 if (strcmp($latest, $client)) { 
 	print "<div class='update-notifier'>
 		<div class='update-logo'>
-			<img src='$themePath/inc/update-oxygen-logo.png' width='50'>
+			<img src='$themePath/inc/components/img/update-oxygen-logo.png' width='50' alt='OxyGen $latest'>
 		</div>
 		<div class='update-text'>
 			<h1>OxyGen Update available!</h1>
 			<p>Your current Version is <strong>$client</strong> and <strong>$latest</strong> is the latest version.</p>
 		</div>
-		<div class='update-button'>
+		<div class='action-button'>
 			<div class='update-download'>
-			<a href='$downloadLink' title='Download $latest'>Download</a>
+			<a href='$downloadLink' title='Download OxyGen $latest'>Download</a>
 			</div>
 			<!--<div class='update-install'>
-				<a href='#' title=''>Install</a>
+				<a href='$updateLink' title='Install OxyGen $latest'>Install</a>
+			</div>
+			<div class='dismiss-update'>
+				<a href='#' title='Dismiss for now'>Dismiss</a>
 			</div>-->
 		</div>
 </div>";
